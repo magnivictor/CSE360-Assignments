1dc79594faaef6875c046d2416963b4db2ac47ff
Ross Hill <rchill9000@gmail.com>
2019-02-25 23:17:42 -0700

    v1 CSE360_assignment2

---
+++ src/cse360assign2/Calculator.java
@@ -0,0 +1,83 @@
+/*
+ * Assignment 2 - CSE360
+ * Author - Ross Hill
+ * ClassID - 405
+ * Description - This file contains methods to act
+ * as a calculator.
+ */
+package cse360assign2;
+
+/**
+ * This class has a methods to add, subtract, multiply,
+ * and divide. It can also return the total integer
+ * calculated and the history of operations.
+ * @see Calculator
+ * @see getTotal
+ * @see add
+ * @see subtract
+ * @see multiply
+ * @see divide
+ * @see getHistory
+ */
+public class Calculator {
+	/*
+	 * Parameter for total.
+	 */
+	private int total;
+	
+	/**
+	 * Constructor that initializes total to size 0.
+	 */
+	public Calculator() {
+		total = 0;
+	}
+	
+	/**
+	 * Gets the total value from private variable and
+	 * returns it.
+	 * @return total value calculated
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	/**
+	 * Adds parameter value to the total value.
+	 * @param value used in addition operation
+	 */
+	public void add(int value) {
+		
+	}
+	
+	/**
+	 * Subtracts parameter value from the total value.
+	 * @param value used in subtraction operation
+	 */
+	public void subtract(int value) {
+		
+	}
+	
+	/**
+	 * Multiplies parameter value by the total value.
+	 * @param value used in multiplication operation
+	 */
+	public void multiply(int value) {
+		
+	}
+	
+	/**
+	 * Divides parameter value by the total value.
+	 * @param value used in division operation
+	 */
+	public void divide(int value) {
+		
+	}
+	
+	/**
+	 * Gets the history of previous operations.
+	 * @return all previous operations as a string
+	 */
+	public String getHistory() {
+		return "";
+	}
+}
